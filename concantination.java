f1a2857497383b2817212f99164577976077c507
Akhila <n.akhila1105@gmail.com>
2026-02-13 14:17:29 +0530

    comminting concantination to coppy1

---
+++ concantination.java
@@ -0,0 +1,13 @@
+public class concantination {
+   public concantination() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = "heloo";
+      String var2 = "how are you";
+      String var3 = var1 + " " + var2;
+      System.out.println("concate the two string: " + var3);
+   }
+}
+    
+
